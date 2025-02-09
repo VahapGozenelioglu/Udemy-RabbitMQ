9ae58bc09f44f68cc3cbe0d8c9eb26a55f524a63
Vahap Gozenelioglu <gozeneliogluvahap@gmail.com>
2025-02-09 17:39:59 +0300

    Implement word queue using publisher and subscriber instances

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+namespace UdemyRabbitMQ.publisher;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri =
+            new Uri("amqps://pprkqvbg:nrvsmOLyMLntI-KS-fmCvlGQUpLq6Pov@rattlesnake.rmq.cloudamqp.com/pprkqvbg");
+
+        using var coonection = await factory.CreateConnectionAsync();
+
+        var channel = await coonection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync("hello-queue", true, false, false);
+
+        Enumerable.Range(1, 60).ToList().ForEach(x =>
+        {
+            string message = $"Message:{x}";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublishAsync(string.Empty, "hello-queue", false, messageBody);  
+            Console.WriteLine("Message sent"); 
+        });
+
+    }
+}
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace UdemyRabbitMQ.subscriber;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri =
+            new Uri("amqps://pprkqvbg:nrvsmOLyMLntI-KS-fmCvlGQUpLq6Pov@rattlesnake.rmq.cloudamqp.com/pprkqvbg");
+
+        using var coonection = await factory.CreateConnectionAsync();
+
+        var channel = await coonection.CreateChannelAsync();
+        await channel.BasicQosAsync(0, 1, false);
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        await channel.BasicConsumeAsync("hello-queue", false, consumer);
+
+        consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Thread.Sleep(2000);
+            Console.WriteLine($"Message: {message}");
+
+            channel.BasicAckAsync(e.DeliveryTag, false);
+            return Task.CompletedTask;
+        };
+
+        Console.ReadLine();
+    }
+}
