090a0d16bce7e6ea11125791c33a1997cd5d34bc
Vahap Gözenelioğlu <gozeneliogluvahap@gmail.com>
2025-02-10 15:42:25 +0300

    Bind subscriber queues to Fanout exchange

--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -6,19 +6,27 @@ namespace UdemyRabbitMQ.subscriber;
 
 class Program
 {
+    private const string ExchangeName = "logs-fanout";
+
     static async Task Main(string[] args)
     {
         var factory = new ConnectionFactory();
         factory.Uri =
             new Uri("amqps://pprkqvbg:nrvsmOLyMLntI-KS-fmCvlGQUpLq6Pov@rattlesnake.rmq.cloudamqp.com/pprkqvbg");
 
-        using var coonection = await factory.CreateConnectionAsync();
+        await using var coonection = await factory.CreateConnectionAsync();
 
         var channel = await coonection.CreateChannelAsync();
-        await channel.BasicQosAsync(0, 1, false);
-        var consumer = new AsyncEventingBasicConsumer(channel);
-        await channel.BasicConsumeAsync("hello-queue", false, consumer);
 
+        var randomQueueName = channel.QueueDeclareAsync().Result.QueueName;
+        await channel.QueueBindAsync(randomQueueName, ExchangeName, "");
+        await channel.BasicQosAsync(0, 1, false);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        await channel.BasicConsumeAsync(randomQueueName, false, consumer);
+
+        Console.WriteLine("Waiting for messages...");
+        
         consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs e) =>
         {
             var message = Encoding.UTF8.GetString(e.Body.ToArray());
