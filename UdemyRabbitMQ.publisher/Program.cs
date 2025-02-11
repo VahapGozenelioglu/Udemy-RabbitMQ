4e37e2cea36ad18c4da273dd758832c64eae1809
Vahap Gözenelioğlu <gozeneliogluvahap@gmail.com>
2025-02-11 15:17:21 +0300

    Implement Direct Exchange

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -5,28 +5,66 @@ using RabbitMQ.Client;
 
 namespace UdemyRabbitMQ.publisher;
 
+public enum LogTypes
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
+
 class Program
 {
-    private const string ExchangeName = "logs-fanout";
+    private const string ExchangeName = "logs-direct";
     static async Task Main(string[] args)
+    {
+        var factory = CreateFactory();
+
+        await using var connection = await factory.CreateConnectionAsync();
+
+        var channel = await connection.CreateChannelAsync();
+        await channel.ExchangeDeclareAsync(ExchangeName, durable: true, type: ExchangeType.Direct); 
+        
+        await DeclareAndBindQueues(channel);
+        await SendMessage(channel);
+    }
+
+    private static async Task DeclareAndBindQueues(IChannel channel)
+    {
+        foreach (var logType in Enum.GetNames(typeof(LogTypes)))
+        {
+            var queueName = $"direct-queue-{logType}";
+            var routingKey = $"route-{logType}";
+        
+            await channel.QueueDeclareAsync(queueName, durable: true, exclusive: false, autoDelete: false);
+            await channel.QueueBindAsync(queueName, ExchangeName, routingKey);
+        }
+    }
+
+    private static ConnectionFactory CreateFactory()
     {
         var factory = new ConnectionFactory();
         factory.Uri =
             new Uri("amqps://pprkqvbg:nrvsmOLyMLntI-KS-fmCvlGQUpLq6Pov@rattlesnake.rmq.cloudamqp.com/pprkqvbg");
-
-        using var coonection = await factory.CreateConnectionAsync();
-
-        var channel = await coonection.CreateChannelAsync();
-
-        await channel.ExchangeDeclareAsync(ExchangeName, durable: true, type: ExchangeType.Fanout); 
-
-        Enumerable.Range(1, 60).ToList().ForEach(x =>
-        {
-            string message = $"Log:{x}";
-            var messageBody = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublishAsync(ExchangeName, "", false, messageBody);  
-            Console.WriteLine($"Message{x} sent"); 
-        });
-
+        return factory;
     }
+
+    private static async Task SendMessage(IChannel channel)
+    {
+        var random = new Random();
+    
+        for (int x = 1; x <= 60; x++)
+        {
+            var logType = (LogTypes)random.Next(1, 5);
+            var message = $"Log type: {logType}";
+            var routeKey = $"route-{logType}";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(ExchangeName, routeKey, false, messageBody);
+            Console.WriteLine($"Log sent {logType}({x})");
+        
+            await Task.Delay(100); // Mesajlar çok hızlı gitmesin
+        }
+    }
+
 }
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -1,42 +1,64 @@
-﻿using System.Text;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace UdemyRabbitMQ.subscriber;
+public enum LogTypes
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
 
 class Program
 {
-    private const string ExchangeName = "logs-fanout";
+    private const string ExchangeName = "logs-direct";
 
     static async Task Main(string[] args)
     {
-        var factory = new ConnectionFactory();
-        factory.Uri =
-            new Uri("amqps://pprkqvbg:nrvsmOLyMLntI-KS-fmCvlGQUpLq6Pov@rattlesnake.rmq.cloudamqp.com/pprkqvbg");
-
+        var factory = CreateFactory();
         await using var connection = await factory.CreateConnectionAsync();
-
         var channel = await connection.CreateChannelAsync();
 
-        var randomQueueName = channel.QueueDeclareAsync().Result.QueueName;
-        await channel.QueueBindAsync(randomQueueName, ExchangeName, "");
+        await StartConsumer(channel);
+    }
+
+    private static ConnectionFactory CreateFactory()
+    {
+        return new ConnectionFactory
+        {
+            Uri = new Uri("amqps://pprkqvbg:nrvsmOLyMLntI-KS-fmCvlGQUpLq6Pov@rattlesnake.rmq.cloudamqp.com/pprkqvbg")
+        };
+    }
+
+    private static async Task StartConsumer(IChannel channel)
+    {
+        var logType = (LogTypes)new Random().Next(1, 5);
+        var queueName = $"direct-queue-{logType}";
+
         await channel.BasicQosAsync(0, 1, false);
 
         var consumer = new AsyncEventingBasicConsumer(channel);
-        await channel.BasicConsumeAsync(randomQueueName, false, consumer);
+        await channel.BasicConsumeAsync(queueName, false, consumer);
 
-        Console.WriteLine("Waiting for messages...");
-        
-        consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs e) =>
+        Console.WriteLine($"Waiting for logs with type: {logType} ...");
+
+        consumer.ReceivedAsync += async (sender, e) =>
         {
             var message = Encoding.UTF8.GetString(e.Body.ToArray());
-            Thread.Sleep(2000);
-            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Received: {message}");
 
-            channel.BasicAckAsync(e.DeliveryTag, false);
-            return Task.CompletedTask;
+            await SaveLogToFileAsync(logType, message);
+            await channel.BasicAckAsync(e.DeliveryTag, false);
         };
 
         Console.ReadLine();
+    }
+
+    private static async Task SaveLogToFileAsync(LogTypes logType, string message)
+    {
+        var filePath = $"log-{logType.ToString().ToLower()}.txt";
+        await File.AppendAllTextAsync(filePath, message + Environment.NewLine);
     }
 }
